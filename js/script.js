9a50456511c0bb377ecf88899c5d98fd4a2b097c
ChetanMorey44 <146196075+ChetanMorey44@users.noreply.github.com>
2024-11-27 15:23:21 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,183 @@
+// const arr =[1,2,3,4,5,6]
+// const a =arr.map((value)=>{
+//     console.log(value)
+//     return value * 2
+// })
+// console.log(a)
+
+// let arr = [1,2,3,4,5, false,'chetan' ,'neha' ]
+
+// console.log(arr[7])
+// arr[5]='true'
+// console.log(arr[5])
+// console.log(arr)
+// console.log(typeof arr)
+
+// console.log(arr.length)
+
+// const chetan = [1,2,3,4,5,]
+// const a = chetan.map((baba)=>{
+//     console.log(baba)
+//     return baba * 10
+// })
+//  console.log(a)
+
+// const chetan = [
+//                 {fname: "chetan" , lname: "Morey"},
+//                 {fname: "Harshvardhan" , lname: "kukle"},
+//                 {fname: "rajesh" , lname: "sapkal"},
+//                 {fname: "harsh" , lname: "yadav"}
+//               ];
+               
+//               const a = chetan.map((x)=>{
+//                     console.log(x)
+//                     return x.fname
+//               })
+//               document.write(a)
+
+// const arr = [1,2,3,5,6,7]
+// const a = arr.map((baba)=>{
+//     // document.write(baba)
+
+//     return baba * 10
+
+// })
+//  document.write(a)
+
+// const arr = [
+//     {fname : 'chetan' , lname : 'morey'},
+//     {fname : "rajesh" , lname : 'sapkal'},
+//     {fname : "rushikesh" , lname : "tayade"}
+//     ]
+
+//      const a = arr.map((baba)=>{
+//         return baba.fname + " " + baba.lname
+//      })
+
+    //  console.log(a)
+    //  document.write(a)
+
+
+    
+
+
+    
+
+    
+
+     
+
+
+
+    // console.log(arr[0])
+    // console.log(arr[1])
+    // console.log(arr[2])
+
+    // const  arrr = [20,30,40,50]
+    // console.log(arrr.length)
+    //  console.log(arrr[0])
+    //  arrr[0]=25
+    //  document.write(arrr)
+    //  arrr[4]=70
+    //  console.log(arrr)
+
+// const a3 = [20,30,40,50,60]
+// const a = a3.filter((y)=>{
+//    return y > 40
+// })
+// document.write(a)
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+//  console.log(a);
+// const arr3 = arr2.map((value)=>{
+//     return value.fname + value.lname;
+// })
+// document.write(arr3[0])
+
+let obj = {
+    name: 'chetan',
+    age : 24,
+    gender : 'male',
+    address :'vilas nagar amravati'
+}
+let {name:names,age:age,gender:male,address:address} = obj 
+//  document.write(age);
+
+const arr = [
+    {fname:'chetan',lname:'morey'},
+    {fname:'rajesh',lname:"sapkal"},
+    {fname:'neha', lname: 'kharsan'}
+]
+// console.log("my name is :" + arr[2].fname)
+// document.write(arr[2].fname)
+// document.write(arr2)
+  
+const array = [1,20,30,40,50,60];
+  for (i=0;i<=array.length;i++){
+    //   console.log(array[i])
+  }
+array.forEach((chetan)=>{
+    // console.log(chetan)
+})
+
+const array6 = [
+    {fname:'chetan',lname:'Morey'},
+    {fname:'chetan',lname:'soraj'},
+    {fname:'eraj', lname:'dinesh'}
+]
+
+// const arrr= {
+//     name:'chetan Morey',
+//     age: 21,
+//     job:'ECTI'
+// }
+
+// let{name:myname,age:myage,job:myjob}=arrr
+// document.write('my name is ' + myname + " and my age is " + myage + " and i am working in as a assisten " + myjob )
+// console.log('my name is' + myname + "and my age is " + myage + "and my educations is" + myjob )
+//   console.log("my name is " + fname + "  "+lname + " and my age is" + myage)
+
+//  console.log(a)
+// document.write(a)
+
+
+
+// const array7 = [10,20,30,40,50];
+//   for(i=0; i<=array7.length; i++){
+//     console.log(array[i])
+
+//     //start//
+
+//   const programming_lang= [ 'js','php','python','css','bootstrap']            
+
+
+
+
+   
+
+ 
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
